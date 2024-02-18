29ec2b010ebb1969ac2f00334662935b3c4c17e0
Николай <nikolyae2019@gmail.com>
2024-02-18 23:24:16 +0300

    Добавил tailwind. Сверстал шапку

--- emails/meister-email.jsx
+++ emails/meister-email.jsx
@@ -1,8 +1,32 @@
-import {Container, Row, Img, Column, Button, Html, Body, Head, Link} from "@react-email/components";
+import {Container, Row, Img, Column, Button, Html, Body, Head, Link, Text, Tailwind} from "@react-email/components";
 import * as React from "react";
 
+const tailwindSettings = {
+    fontFamily: {
+        base: 'ProximaNova, Tahoma, Arial, sans-serif'
+    },
+    fontWeight: {
+        light: 300,
+        regular: 400,
+    },
+    extend: {
+        spacing: {
+            '1': '1px',
+            '2': '2px',
+            '3': '3px',
+            '4': '4px',
+            '5': '5px',
+            '6': '6px',
+            '7': '7px',
+            '8': '8px',
+            '9': '9px',
+            '10': '10px'
+        }
+    }
+}
 
 const currentPhone = "+79991205982";
+const currentPhoneBeautyfied = "+7 (999) 120 59 82"
 const currentName = "Николай";
 const currentEmail = "nickolasdzr@yandex.ru";
 const currentSiteUrl = "https://tk-meister.ru/"
@@ -13,54 +37,80 @@ const baseUrl = process.env.VERCEL_URL
 
 export default function Email() {
     return (
-        <Html>
-            <Head>
-                <title>Коммерческое предложение ООО "Мейстер"</title>
-                <link rel="stylesheet" href={currentSiteUrl + "email-assets/meister-email-fonts.css"}/>
-            </Head>
-            <Body style={main}>
-                <Container>
-                    <Row>
-                        <Column style={col4}>
-                            <Link href={`tel:${currentPhone}`} style={{...linkReset, ...linkTopPhone}}>
+        <Tailwind config={{
+            theme: tailwindSettings,
+        }}>
+            <Html>
+                <Head>
+                    <title>Коммерческое предложение ООО "Мейстер"</title>
+                    <link rel="stylesheet" href={currentSiteUrl + "email-assets/meister-email-fonts.css"}/>
+                </Head>
+                <Body className="bg-white font-light font-base">
+                    <Container className="font-base bg-white font-light pt-5">
+                        <Row>
+                            <Column style={col4}>
+                                <Link href={`tel:${currentPhone}`} className="font-regular text-black inline">
+                                    <Img
+                                        src={`${baseUrl}/static/phone.svg`}
+                                        alt="картинка телефона"
+                                        className="inline align-top mr-7"
+                                        width="16"
+                                        height="16"
+                                    />
+
+                                    {currentPhoneBeautyfied}
+                                </Link>
+                            </Column>
+                            <Column style={col4}>
+                                <Link href={currentSiteUrl} style={linkLogo}>
+                                    <Img
+                                        src={`../../static/logo.png`}
+                                        width="163"
+                                        height="56"
+                                        alt="Логотип ООО 'Мейстер'"
+                                        style={mainLogo}
+                                    />
+                                </Link>
+                            </Column>
+                            <Column style={col4}>
+                                <Link href={`mailto:${currentEmail}`} style={{...linkReset, ...linkTopEmail}}>
+                                    {currentEmail}
+                                    <Img
+                                        src={`${baseUrl}/static/mail.svg`}
+                                        width="16"
+                                        height="16"
+                                        className="inline align-middle ml-7"
+                                        alt="картинка телефона"
+                                    />
+                                </Link>
+                            </Column>
+                        </Row>
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Column>
                                 <Img
-                                    src={`${baseUrl}/static/phone.svg`}
+                                    src={`${baseUrl}/static/main-photo.png`}
+                                    width="640"
+                                    height="341"
+                                    className="inline align-middle max-w-full"
                                     alt="картинка телефона"
-                                    style={topHeadPhoneSvg}
-                                    width="16"
-                                    height="16"
                                 />
-                                {currentPhone}
-                            </Link>
-                        </Column>
-                        <Column style={col4}>
-                            <Link href={currentSiteUrl} style={linkLogo}>
-                                <Img
-                                    src={`../../static/logo.png`}
-                                    width="163"
-                                    height="56"
-                                    alt="Логотип ООО 'Мейстер'"
-                                    style={mainLogo}
-                                />
-                            </Link>
-                        </Column>
-                        <Column style={col4}>
-                            <Link href={`mailto:${currentEmail}`} style={{...linkReset, ...linkTopEmail}}>
-                                {currentEmail}
-                                <Img
-                                    src={`${baseUrl}/static/mail.svg`}
-                                    width="16"
-                                    height="16"
-                                    alt="картинка телефона"
-                                    style={topHeadEmailSvg}
-                                />
-                            </Link>
-                        </Column>
-                    </Row>
-                </Container>
-            </Body>
-        </Html>
-    );
+                            </Column>
+                        </Row>
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Column>
+                                <Text className="text-2xl font-bold">Уважаемые партнеры!</Text>
+                                <Text>Наша компания специализируется на перевозке грузов по всей России и её регионам. Нам доверяют клиенты, благодарят за качественную работу и обращаются снова и снова.</Text>
+                                <Text>Наша компания специализируется на перевозке грузов по всей России и её регионам. Нам доверяют клиенты, благодарят за качественную работу и обращаются снова и снова.</Text>
+                            </Column>
+                        </Row>
+                    </Container>
+                </Body>
+            </Html>
+        </Tailwind>);
 }
 
 const col4 = {
@@ -83,31 +133,14 @@ const linkLogo = {
     textAlign: "center",
 }
 
-const topHeadPhoneSvg = {
-    display: "inline",
-    verticalAlign: "top",
-    marginRight: "7px"
-}
-
-const topHeadEmailSvg = {...topHeadPhoneSvg};
-
-topHeadEmailSvg["marginRight"] = "0";
-topHeadEmailSvg["marginLeft"] = "7px";
-topHeadEmailSvg["verticalAlign"] = "bottom";
-
 const linkReset = {
     textDecoration: "none",
     color: "black",
     display: "inline-block",
 }
 
-const linkTopPhone = {
-    width: "100%",
-    textAlign: "left",
-    fontWeight: 400,
-}
-
 const linkTopEmail = {
     width: "100%",
     textAlign: "right",
+    fontWeight: 400,
 }
